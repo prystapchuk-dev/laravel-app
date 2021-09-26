99f1b44c09b7d967b39b7494616654837f7d832b
prystapchuk-dev <85236924+prystapchuk-dev@users.noreply.github.com>
2021-09-26 16:20:04 +0300

    install laravel, debagbar, ide-helper

--- config/app.php
+++ config/app.php
@@ -161,6 +161,10 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+
+
 
         /*
          * Package Service Providers...
@@ -228,6 +232,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+
 
     ],
 
